7460054adf0a731e30d000bbf21de8c1ab531c28
Zoro Zorec <zoro.zorec@endava.com>
2021-06-07 10:37:51 +0200

    t

--- ot-recorder/data/run.sh
+++ ot-recorder/data/run.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# If running as root (first invocation), fix mountpoint permissions
-# and re-run this script as appuser.
-if [[ $(id -u) -eq 0 ]]; then
-  chown -R appuser:appuser /store /config
-  chown appuser:appuser /makeconf.sh
-  exec su appuser -- "$0" "$@"
-fi
-
 echo "Make Config File"
 CONFIG_FILE=/data/options.json
 CONFIG_OUT=/config/recorder.conf
