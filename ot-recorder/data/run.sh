37529044abb7acceb2279f3697f88723375dec0c
Zoro Zorec <zoro.zorec@endava.com>
2021-06-07 10:29:02 +0200

    t

--- ot-recorder/data/run.sh
+++ ot-recorder/data/run.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# If running as root (first invocation), fix mountpoint permissions
+# and re-run this script as appuser.
+if [[ $(id -u) -eq 0 ]]; then
+  chown -R appuser:appuser /store /config
+  chown appuser:appuser /makeconf.sh
+  exec su appuser -- "$0" "$@"
+fi
+
+echo "Make Config File"
 CONFIG_FILE=/data/options.json
 CONFIG_OUT=/config/recorder.conf
 
@@ -17,3 +26,8 @@ OUTPUT=$OUTPUT"OTR_PASS = \"$(jq --raw-output '.mqttPassword' $CONFIG_FILE)\"\n"
 OUTPUT=$OUTPUT"OTR_TOPICS = \"$(jq --raw-output '.topic' $CONFIG_FILE)\""
 
 echo -e $OUTPUT >> $CONFIG_OUT
+
+echo $OUTPUT
+
+ot-recorder --initialize
+ot-recorder "$@"
