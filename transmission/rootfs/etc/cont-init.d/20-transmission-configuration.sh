1c9fbf7b072e995715461d3e229be850911704c0
zoroz <zoro.zorec@endava.com>
2022-08-07 15:59:45 +0200

    jellyfin

---
+++ transmission/rootfs/etc/cont-init.d/20-transmission-configuration.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+
+declare CONFIG
+declare authentication_required
+declare username
+declare password
+declare download_dir
+declare incomplete_dir
+
+if ! bashio::fs.directory_exists '/data/transmission'; then
+  mkdir '/data/transmission'
+fi
+
+if ! bashio::fs.file_exists '/data/transmission/settings.json'; then
+  echo "{}" > /data/transmission/settings.json
+fi
+
+CONFIG=$(</data/transmission/settings.json)
+
+# Defaults
+incomplete_dir=$(bashio::config 'incomplete_dir')
+download_dir=$(bashio::config 'download_dir')
+CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir\"=\"${incomplete_dir}\"")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"incomplete-dir-enabled\"=true")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"download-dir\"=\"${download_dir}\"")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-whitelist-enabled\"=false")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-host-whitelist-enabled\"=false")
+CONFIG=$(bashio::jq "${CONFIG}" ".\"bind-address-ipv4\"=\"0.0.0.0\"")
+
+authentication_required=$(bashio::config 'authentication_required')
+CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-authentication-required\"=${authentication_required}")
+
+
+username=$(bashio::config 'username')
+CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-username\"=\"${username}\"")
+
+
+password=$(bashio::config 'password')
+CONFIG=$(bashio::jq "${CONFIG}" ".\"rpc-password\"=\"${password}\"")
+
+echo "${CONFIG}" > /data/transmission/settings.json
